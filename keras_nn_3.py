4fb646b9a54452c507402106d6e0a05966f5ff96
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2023-09-20 22:23:10 -0600

    Add files via upload

---
+++ keras_nn_3.py
@@ -0,0 +1,5 @@
+model = keras.models.Sequential()
+model.add(Dense(2, input_dim=2, activation='relu'))
+model.add(Dense(3, activation='relu'))
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mse')
